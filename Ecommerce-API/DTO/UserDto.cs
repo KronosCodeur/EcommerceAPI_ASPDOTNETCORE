426d1d433654d6b8128f71f6daf91c45de0b5e4c
Kronos_Codeur <piraliarris@gmail.com>
2023-12-29 20:52:45 +0000

    userDTO update

--- Ecommerce-API/DTO/UserDto.cs
+++ Ecommerce-API/DTO/UserDto.cs
@@ -4,4 +4,6 @@ public class UserDto
 {
     public string Username { get; set; }
     public string Password { get; set; }
+    public string Address { get; set; }
+    public string Email { get; set; }
 }
