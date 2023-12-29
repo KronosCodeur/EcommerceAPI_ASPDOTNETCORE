a654e42c54cb13f9ce151f64d751e276e0b9f19a
Kronos_Codeur <piraliarris@gmail.com>
2023-12-29 20:35:44 +0000

    initial project

---
+++ Ecommerce-API/Program.cs
@@ -0,0 +1,32 @@
+using Ecommerce_API.Data;
+using Ecommerce_API.Services;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<UserDbContext>(opt =>
+    opt.UseNpgsql(builder.Configuration.GetConnectionString("ECommerceConnectionString")));
+builder.Services.AddScoped<UserService>();
+builder.Services.AddAutoMapper(typeof(Program));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
