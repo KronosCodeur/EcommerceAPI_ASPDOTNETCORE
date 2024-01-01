ffb82da8e11b3585f5bfdad8908090eb36d7b48f
Kronos_Codeur <piraliarris@gmail.com>
2024-01-01 14:30:46 +0000

    ProductRepository done

--- Ecommerce-API/Repositories/ProductRepository.cs
+++ Ecommerce-API/Repositories/ProductRepository.cs
@@ -1,6 +1,11 @@
+using Ecommerce_API.Models;
+
 namespace Ecommerce_API.Repositories;
 
 public interface IProductRepository
 {
+    Task<bool> CreateProduct(Product product);
+    Task<List<Product>> GetAllProducts();
+    Task<Product> ProductInfo(int id);
 
 }
--- Ecommerce-API/Services/ProductService.cs
+++ Ecommerce-API/Services/ProductService.cs
@@ -1,8 +1,32 @@
+using Ecommerce_API.Data;
+using Ecommerce_API.Models;
 using Ecommerce_API.Repositories;
 
 namespace Ecommerce_API.Services;
 
 public class ProductService : IProductRepository
 {
+    private readonly DatabaseContext _databaseContext;
 
+    public ProductService(DatabaseContext databaseContext)
+    {
+        _databaseContext = databaseContext;
+    }
+
+    public async Task<bool> CreateProduct(Product product)
+    {
+        await _databaseContext.AddAsync(product);
+        await _databaseContext.SaveChangesAsync();
+        return true;
+    }
+
+    public async Task<List<Product>> GetAllProducts()
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<Product> ProductInfo(int id)
+    {
+        throw new NotImplementedException();
+    }
 }
