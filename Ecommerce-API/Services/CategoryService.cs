f12945868b7f1643f973b75d467c27ba5e2c61db
Kronos_Codeur <piraliarris@gmail.com>
2024-01-01 10:05:27 +0000

    Category repository service done

---
+++ Ecommerce-API/Services/CategoryService.cs
@@ -0,0 +1,29 @@
+using Ecommerce_API.Data;
+using Ecommerce_API.Models;
+using Ecommerce_API.Repositories;
+using Microsoft.EntityFrameworkCore;
+
+namespace Ecommerce_API.Services;
+
+public class CategoryService : ICategoryRepository
+{
+    private readonly DatabaseContext _databaseContext;
+
+    public CategoryService(DatabaseContext databaseContext)
+    {
+        _databaseContext = databaseContext;
+    }
+
+    public async Task<Category> CreateCategory(Category category)
+    {
+        await _databaseContext.Categories.AddAsync(category);
+        await _databaseContext.SaveChangesAsync();
+        return category;
+    }
+
+    public async Task<List<Category>> GetAllCategories()
+    {
+        var categories = await _databaseContext.Categories.ToListAsync();
+        return categories;
+    }
+}
