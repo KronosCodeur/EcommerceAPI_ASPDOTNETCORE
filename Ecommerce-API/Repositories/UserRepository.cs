aee06c34081f225a89458dab507152757da263f2
Kronos_Codeur <piraliarris@gmail.com>
2024-01-03 13:00:40 +0000

    Token validation method

--- Ecommerce-API/Repositories/UserRepository.cs
+++ Ecommerce-API/Repositories/UserRepository.cs
@@ -7,5 +7,6 @@ public interface IUserRepository
     Task<bool> Register(User user);
     Task<User?> Login(string username);
     string CreateUserToken(User user);
+    Task<bool> IsTokenValid(string token);
 
 }
